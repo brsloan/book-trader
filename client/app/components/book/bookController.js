5a1ccdaa5d713cfaa1d6e492ace636e5806c2213
Benjamin Sloan <brsloan@gmail.com>
2016-02-02 05:54:17 +0000

    Added tooltips and alt text for cover images

--- client/app/components/book/bookController.js
+++ client/app/components/book/bookController.js
@@ -10,7 +10,6 @@ angular.module('bookTrader')
       function($scope, auth, user, book, reqBook, trade, $state){
         var formData = {};
         $scope.formData = formData;
-        //$scope.books = book.myBooks;
         $scope.reqBook = reqBook;
         
         $scope.isLoggedIn = auth.isLoggedIn;
